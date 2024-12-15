2fd1c4a6c43f400047f5288e3f38cbbe4c24dbd8
muhammedarshadtp <arshadtp89@gmail.com>
2024-12-15 07:24:49 +0530

    i was done by search cache

---
+++ src/utils/ApiContants.jsx
@@ -0,0 +1,8 @@
+
+
+
+
+export const YOUTUBE_VIDEOAPI="https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+import.meta.env.VITE_GOOGLE_API;
+
+export const YOUTUBE_SEARCH_API=
+        "https://thingproxy.freeboard.io/fetch/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
